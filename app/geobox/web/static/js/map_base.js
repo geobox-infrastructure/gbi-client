c58f6433877cc09f016f9892dbc7211efa017045
Dominik Helle <helle@omniscale.de>
2012-11-23 08:10:16 +0100

    add styling options for vector layer - default color is red now

--- app/geobox/web/static/js/map_base.js
+++ app/geobox/web/static/js/map_base.js
@@ -76,16 +76,35 @@ function activate_draw_controls(map) {
 
     var sketchSymbolizers = {
          "Point": {
-            pointRadius: 10
-        }
-    };
+            pointRadius: 8,
+            fillColor: "#ccc",
+            fillOpacity: 1,
+            strokeWidth: 1,
+            strokeOpacity: 1,
+            strokeColor: "#D6311E"
+          },
+          "Line": {
+            strokeWidth: 3,
+            strokeOpacity: 1,
+            strokeColor: "#D6311E",
+            strokeDashstyle: "dash"
+          },
+          "Polygon": {
+            strokeWidth: 2,
+            strokeOpacity: 1,
+            strokeColor: "#D6311E",
+            fillColor: "#D6311E",
+            fillOpacity: 0.6
+          }
+      };
 
     var style = new OpenLayers.Style();
-        style.addRules([
+    style.addRules([
         new OpenLayers.Rule({symbolizer: sketchSymbolizers})
     ]); 
-    var styleMap = new OpenLayers.StyleMap({"default": style});
-
+    var styleMap = new OpenLayers.StyleMap(
+        {"default": style}
+    );
 
     selected_feature = null;
     var draw_layer = new OpenLayers.Layer.Vector("Draw Layer", {
