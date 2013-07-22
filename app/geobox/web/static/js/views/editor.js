9b99d9bed680f09d7cfcf4be55abfe3ac8f2d81f
Kai Culemann <culemann@omniscale.de>
2013-07-22 15:10:10 +0200

    Replaced "delete all features" with "select all features"

--- app/geobox/web/static/js/views/editor.js
+++ app/geobox/web/static/js/views/editor.js
@@ -25,18 +25,14 @@ $(document).ready(function() {
         }
    });
 
-   $('#remove_all_features').click(function() {
+   $('#select_all_features').click(function() {
         var activeLayer = editor.layerManager.active();
         if (!activeLayer) {
-          $('#deleteAllGeometries').modal('hide');
           return false;
         }
         if(activeLayer instanceof gbi.Layers.SaveableVector) {
-            activeLayer.unSelectAllFeatures();
-            editor.map.toolbars[1].delete_.olControl.deleteFeatures(activeLayer.olLayer.features)
-            activeLayer.changesMade();
+            activeLayer.selectAllFeatures();
         }
-        $('#deleteAllGeometries').modal('hide');
         return false;
    });
 
