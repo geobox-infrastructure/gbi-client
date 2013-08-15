dea1811488d962b25949b340c4130fa9be4a47a0
Dominik Helle <helle@omniscale.de>
2013-08-15 10:45:43 +0200

    add preview option to box and editor

--- app/geobox/web/views/map.py
+++ app/geobox/web/views/map.py
@@ -17,8 +17,9 @@ from flask import Blueprint, render_template, request, g, current_app
 
 from ..helper import get_local_cache_url
 from geobox.model import LocalWMTSSource, ExternalWMTSSource
-from geobox.lib.couchdb import CouchDB, vector_layers_metadata
+from geobox.lib.couchdb import CouchDB, CouchFileBox, vector_layers_metadata
 from geobox.web.forms import ExportVectorForm
+from .boxes import get_couch_box_db
 
 map_view = Blueprint('map_view', __name__)
 
@@ -59,6 +60,18 @@ def editor():
     upload_box = current_app.config.geobox_state.config.get('couchdb', 'upload_box')
     export_form.destination.choices = [('file', 'Filesystem'), (upload_box, upload_box)]
 
+    # load preview layer
+    preview_features = False
+    preview_layername = False
+
+    box_name = request.args.get('box_name', False)
+    filename = request.args.get('filename', False)
+    if box_name and filename:
+        couchbox = get_couch_box_db(box_name)
+        couch_src = CouchFileBox('http://%s:%s' % ('127.0.0.1', current_app.config.geobox_state.config.get('couchdb', 'port')), couchbox)
+        preview_features = couch_src.get_attachment(filename)
+        preview_layername = "tmp_%s" % (filename)
+
     raster_sources = g.db.query(LocalWMTSSource).all()
     base_layer = g.db.query(ExternalWMTSSource).filter_by(background_layer=True).first()
     base_layer.bbox = base_layer.bbox_from_view_coverage()
@@ -73,6 +86,8 @@ def editor():
         couch_layers=couch_layers,
         sources=raster_sources,
         export_form=export_form,
+        preview_layername=preview_layername,
+        preview_features=preview_features,
     )
 
 
