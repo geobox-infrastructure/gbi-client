f3e03d70083746b198a8e5f722b9a03d5f4eeeb7
Kai Culemann <culemann@omniscale.de>
2013-09-11 15:18:48 +0200

    Add format to wmts form

--- app/geobox/web/forms.py
+++ app/geobox/web/forms.py
@@ -248,10 +248,10 @@ class RasterSourceForm(Form):
     name = TextField(lazy_gettext('rastersource_name'), validators=[Required(), Regexp('[a-z0-9_-]+$')])
     title = TextField(lazy_gettext('rastersource_title'), validators=[Required()])
     layer = TextField(lazy_gettext('rastersource_layer_input'))
+    format = SelectField(lazy_gettext('rastersource_format'), validators=[Required()], choices=[('png', 'png'), ('jpeg', 'jpeg')])
     llbbox = TextField(lazy_gettext('rastersource_llbox'))
 
 class WMSForm(RasterSourceForm):
-    format = SelectField(lazy_gettext('rastersource_format'), validators=[Required()], choices=[('png', 'png'), ('jpeg', 'jpeg')])
     srs = TextField(lazy_gettext('rastersource_srs_input'))
     version = SelectField(lazy_gettext('wms_version'), choices=[('1.1.1', '1.1.1'), ('1.3.0', '1.3.0')],
         validators=[Required()])
