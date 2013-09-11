0a338635a0626420997dc53c7f41c6cdbb6c24b8
Kai Culemann <culemann@omniscale.de>
2013-09-11 12:17:11 +0200

    Add also title to cloned couch layer

--- app/geobox/web/static/js/views/editor.js
+++ app/geobox/web/static/js/views/editor.js
@@ -149,17 +149,19 @@ $(document).ready(function() {
     //  savepoint settings end
 
     $('#save-as').click(function() {
-      var newName = $('#save-as-name').val();
+      var newTitle = $('#save-as-name').val();
+      var newName = 'local_vector_' + newTitle;
       if(newName && activeLayer) {
         var newLayer = false;
         // clone couch if class is couch
         if (activeLayer.CLASS_NAME == 'gbi.Layers.Couch') {
-          newLayer = activeLayer.clone(newName, true);
+          newLayer = activeLayer.clone(newName, true, newTitle);
           newLayer.visible(true);
         } else {
           //  create couch to save layer and copy features
           newLayer = new gbi.Layers.Couch({
               name: newName,
+              title: newTitle,
               url: OpenlayersCouchURL,
               displayInLayerSwitcher: true,
               createDB: false,
