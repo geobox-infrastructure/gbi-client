99dd9324f3885f8a64e2fc9f41bec817ffd625dd
Kai Culemann <culemann@omniscale.de>
2013-10-29 09:49:35 +0100

    Error handling in push_couchapp

--- app/geobox/lib/offline.py
+++ app/geobox/lib/offline.py
@@ -4,6 +4,8 @@ import requests
 import os
 import mimetypes
 from flask import render_template
+from requests.sessions import InvalidSchema
+from requests.models import MissingSchema
 from geobox.appstate import GeoBoxState
 from geobox.defaults import GeoBoxConfig
 from geobox.web import create_app
@@ -30,7 +32,11 @@ def static_files(basedir, attachments=None):
 
 
 def push_couchapp(attachments, couchurl, appname):
-    resp = requests.put(couchurl)
+    try:
+        resp = requests.put(couchurl)
+    except (MissingSchema, InvalidSchema), ex:
+        log.exception(ex)
+        return False
     if not resp.status_code in [201, 412]:
         log.warn('faild to create database %s', couchurl)
         return False
