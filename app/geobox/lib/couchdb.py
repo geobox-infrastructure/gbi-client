314d567b544b6654a70c9006ffc8e2ccc0e4a5b6
Oliver Tonnhofer <olt@bogosoft.com>
2015-01-28 11:00:36 +0100

    do not convert properties to datetimes

--- app/geobox/lib/couchdb.py
+++ app/geobox/lib/couchdb.py
@@ -465,10 +465,6 @@ class VectorCouchDB(CouchDBBase):
                                         if(row.value.hasOwnProperty(prop)){
                                             if(typeof(row.value[prop]) === "number" || /[-+]?[0-9]*\.?[0-9]+/.test(row.value[prop])) {
                                                 send('<d:'+prop+' m:type="Edm.Double">'+row.value[prop]+'</d:'+prop+'>');
-                                            } else if (!isNaN(Date.parse(row.value[prop]))) {
-                                                var d = Date.parse(row.value[prop]);
-                                                var date = new Date(d).toUTCString();
-                                                send('<d:'+prop+' m:type="Edm.DateTime">'+date+'</d:'+prop+'>');
                                             } else {
                                                 send('<d:'+prop+' m:type="Edm.String">'+row.value[prop]+'</d:'+prop+'>');
                                             }
